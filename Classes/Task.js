251c5f22e4d41fdbfcbd8074f38c7025e688dea6
James Drawneek <jmdrawneek@googlemail.com>
2018-01-12 14:32:39 +0000

    Added reporting to automated testing.

--- Classes/Task.js
+++ Classes/Task.js
@@ -4,6 +4,10 @@ const path = require('path');
 
 module.exports = class Task {
   constructor(config) {
+    // Setup environment variables for passing data back from the tests.
+    process.env.testData = {
+      browserVersion: null
+    };
   }
 
   runTests(framework) {
@@ -28,7 +32,7 @@ module.exports = class Task {
 
   runCucumber() {
     return new Promise((resolve, reject) => {
-      const cucumber = spawn(`node`, ['node_modules/cucumber/bin/cucumber'], {
+      const cucumber = spawn(`node`, ['node_modules/cucumber/bin/cucumber', '-f', 'json:cucumber_report.json'], {
         cwd: path.join(__dirname, '../../..')
       });
 
@@ -51,5 +55,29 @@ module.exports = class Task {
 
   reportCucumber() {
 
+    const reporter = require('cucumber-html-reporter');
+
+    let osType = os.type();
+    let osVersion = os.release();
+    osType = (osType === 'Darwin') ? process.env._system_name : osType;
+    osVersion = (osType === 'OSX') ? process.env._system_version : osVersion;
+
+    let options = {
+      theme: 'bootstrap',
+      jsonFile: 'cucumber_report.json',
+      output: 'cucumber_report.html',
+      reportSuiteAsScenarios: true,
+      launchReport: true,
+      metadata: {
+        "App Version": process.env.npm_package_dependencies_cucumber,
+        "Test Environment": "STAGING",
+        "Browser": process.env.testData.browserVersion,
+        "Platform": osType + ' ' + osVersion,
+        "Parallel": "Scenarios",
+        "Executed": "Local"
+      }
+    };
+
+    reporter.generate(options);
   }
 }
